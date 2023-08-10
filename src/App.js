039d87e226316dd54b7d9b31ed69bce1523d60bd
GP LEE <gl1144@nyu.edu>
2023-08-10 10:58:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from "./components/Form";
 function App() {
   return (
     <div className="wrapper">
-      <h1>Todo List</h1>
+      <h1>Test Stacy List</h1>
       <Form/>
     </div>
   );

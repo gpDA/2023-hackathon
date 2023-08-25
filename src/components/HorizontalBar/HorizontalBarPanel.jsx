2f58d549c6215047d00b8b81a2fc4c0ada782aa6
gpDA <gl1144@nyu.edu>
2023-08-25 17:01:50 -0400

    make a Cell component

--- src/components/HorizontalBar/HorizontalBarPanel.jsx
+++ src/components/HorizontalBar/HorizontalBarPanel.jsx
@@ -1,9 +1,23 @@
-import './HorizontalBarPanel.scss'
+import './HorizontalBarPanel.scss';
+import Cell from '../Cell/Cell';
+import ToggleButton from '../utils/ToggleButton/ToggleButton';
 
 const HorizontalBarPanel = () => {
+    const toggleCB = toggleState => {
+        // false | true
+    }
+
     return (
         <div className="horizontal-bar-left">
             hello HorizontalBarPanel
+
+            <Cell title={'cell-title'}>
+                <ToggleButton 
+                    label="Toggle me"
+                    toggled={true}
+                    onClick={toggleCB}
+                />
+            </Cell>
         </div>
     )
 };

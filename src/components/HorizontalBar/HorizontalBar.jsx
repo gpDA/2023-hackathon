b6f3936eae46cb98ff786e9886149d2c4d84b0b1
gpDA <gl1144@nyu.edu>
2023-09-07 15:10:10 -0400

    [hack-17] create rotate control for HorizontalBar

--- src/components/HorizontalBar/HorizontalBar.jsx
+++ src/components/HorizontalBar/HorizontalBar.jsx
@@ -23,12 +23,6 @@ const HorizontalBar = ({
       .padding(.1);
   }, [data]);     
 
-
-  // X-axis
-  const x = d3.scaleLinear()
-  .domain([0, 100])
-  .range([ 0, width]);
-
   // idea for the customization
   // TODO: set the max for each ticks
   // TODO: change the margin for x- & y- axis
@@ -41,15 +35,35 @@ const HorizontalBar = ({
     heightLength: height + margin + 100,
     svgTransform: `translate(${margin + 30},${margin / 2})`,
     axisY: y,
-    axisX: x,
+    axisX: null, // x - x cannot be initialized before rotateAttr useState
     axisXFunctionName: 'axisBottom',
+    axisYFunctionName: 'axisLeft',
     axisYTransform: "translate(0," + height + ")",
     axisXTransform: "translate(10,10)rotate(45)",
+    axisXLeftRotateTransform: null,
     rectX: function () { return x(0)},
-    rectY: function(d) { return y(d?.Country); },
-    rectWidth: function(d) { return x(d?.Value); },
+    rectY: function(d) { return y(d.Country); },
+    rectWidth: function(d) { return x(d.Value); },
     rectHeight: function () { return y.bandwidth()},
   })
+
+  // X-axis
+
+  const x = useMemo(() => {
+    const values = data.map(ele => ele.Value)
+    let xLinearDomainRange = [0, d3.max(values)];
+
+    if (rotateId === 3 || rotateId === 2) {
+      xLinearDomainRange = [d3.max(values), 0]
+    } else {
+      xLinearDomainRange = [0, d3.max(values)] // [0, 100]
+    }
+    return d3.scaleLinear()
+    .domain(xLinearDomainRange)
+    .range([ 0, width])
+  }, [rotateId]);
+
+  
 
   const rotateButtonGroupCB = (name) => {
     setRotateId(name)
@@ -69,8 +83,10 @@ const HorizontalBar = ({
       attr.axisY = y;
       attr.axisX = x;
       attr.axisXFunctionName = 'axisBottom',
+      attr.axisYFunctionName = 'axisLeft',
       attr.axisYTransform = "translate(0," + height + ")";
       attr.axisXTransform = "translate(10,10)rotate(45)";
+      attr.axisXLeftRotateTransform = null;
       attr.rectX = function () { return x(0)};
       attr.rectY = function(d) { return y(d.Country); };
       attr.rectWidth = function(d) { return x(d.Value); };
@@ -83,12 +99,46 @@ const HorizontalBar = ({
       attr.axisY = x;
       attr.axisX = y;
       attr.axisXFunctionName = 'axisTop',
+      attr.axisYFunctionName = 'axisLeft',
       attr.axisYTransform = "translate(0, 0)";
       attr.axisXTransform = "translate(+10,-10)rotate(0)";
+      attr.axisXLeftRotateTransform = null;
       attr.rectX = function(d) { return y(d.Country); };
       attr.rectY = function () { return x(0)};
       attr.rectWidth = function () { return y.bandwidth()};
       attr.rectHeight = function(d) { return x(d.Value); };
+    }
+    if (rotateId === 2) {
+      attr.widthLength = width + margin + 100;
+      attr.heightLength = height + margin + 100;
+      attr.svgTransform = `translate(${margin + 30},${margin / 2})`;
+      attr.axisY = y;
+      attr.axisX = x;
+      attr.axisXFunctionName = 'axisBottom',
+      attr.axisYFunctionName = 'axisRight',
+      attr.axisYTransform = "translate(0," + height + ")";
+      attr.axisXTransform = "translate(10,10)rotate(45)";
+      attr.axisXLeftRotateTransform = `translate(${width}, 0)`;
+      attr.rectX = function (d) { return x(d.Value)};
+      attr.rectY = function(d) { return y(d.Country); };
+      attr.rectWidth = function(d) { return width - x(d.Value); };
+      attr.rectHeight = function () { return y.bandwidth()};
+    }
+    if (rotateId === 3) {
+      attr.widthLength = height + margin + 100;
+      attr.heightLength = width + margin + 100;
+      attr.svgTransform = `translate(${margin + 30},${margin / 2 + 60})`;
+      attr.axisY = x;
+      attr.axisX = y;
+      attr.axisXFunctionName = 'axisBottom',
+      attr.axisYFunctionName = 'axisRight',
+      attr.axisYTransform = `translate(0, ${height+100})`;
+      attr.axisXTransform = "translate(+10,0)rotate(0)";
+      attr.axisXLeftRotateTransform = `translate(${width-100}, 0)`;
+      attr.rectX = function(d) { return y(d.Country); }; // d.Country
+      attr.rectY = function (d) { return x(d.Value) }; // height + 100 - x(d.Value)
+      attr.rectWidth = function () { return y.bandwidth()};
+      attr.rectHeight = function(d) { return height + 100 - x(d.Value) };
     }    
 
     setRotateAttr((previous) => ({
@@ -110,20 +160,23 @@ const HorizontalBar = ({
 
       return svg;
     },
-    [height, width, margin]
-  ); 
+    [height, width, margin, rotateId]
+  );
 
   const createGraph = (div) => {
     const svg = drawSvg(div);
 
     svg.append("g")
         .attr("transform", rotateAttr.axisYTransform)
-        .call(d3[rotateAttr.axisXFunctionName](rotateAttr.axisX).tickSizeOuter(0))
+        .call(d3[rotateAttr.axisXFunctionName](rotateAttr?.axisX ?? x
+        .range([ 0, width])).tickSizeOuter(0))
         .selectAll("text")
           .attr("transform", rotateAttr.axisXTransform)
           .style("text-anchor", "end");
     svg.append("g")
-        .call(d3.axisLeft(rotateAttr.axisY).tickSizeOuter(0));
+        .call(d3[rotateAttr.axisYFunctionName](rotateAttr.axisY).tickSizeOuter(0))
+        // ${width}
+        .attr("transform", rotateAttr.axisXLeftRotateTransform);
 
     const rec = svg.selectAll("rect").data(data);
 

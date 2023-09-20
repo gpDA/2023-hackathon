c4bf6368c9e7dcad02cd6cc7311aec8307dc3250
stacy cho <stacy.cho@traivefinance.com>
2023-09-20 17:42:40 -0400

    adding hover effect

--- src/components/StackedBar/StackedBar.jsx
+++ src/components/StackedBar/StackedBar.jsx
@@ -7,7 +7,7 @@ import './StackedBar.scss';
 
 const StackedBar = ({
   data,
-  label = { show: true, xLabel: "Percentage %", yLabel: "Fruit Type" },
+  label = { show: true },
   width = 400,
   height = 400,
   margin = 80,
@@ -18,21 +18,43 @@ const StackedBar = ({
 }) => {
   const svgRef = useRef(null);
   const [graphAngle, setGraphAngle] = useState('left');
+  const [labels, setLabels] = useState({
+    xLabel: "Percentage %",
+    yLabel: "Fruit Type"
+  })
+  const [checked, setChecked] = useState(false);
 
+  /* PANEL FUNCTIONS */
   const handleClick = (angle) => {
     setGraphAngle(angle)
   }
+  const handleLabel = (type, value) => {
+    if (type === "xLabel") {
+      setLabels(prev => ({ ...prev, xLabel: value }));
+    }
+    if (type === "yLabel") {
+      setLabels(prev => ({ ...prev, yLabel: value }));
+    }
+  }
 
+  /* SVG functions */
   const drawSvg = useCallback(
     (div) => {
-      d3.selectAll(".test2").remove();
+      d3.selectAll(".svg-content").remove();
       const svg = d3
         .select(div)
         .attr("width", width + margin + 50)
         .attr("height", height + margin + 50)
         .append("g")
-        .attr('class', 'test2')
-        .attr("transform", `translate(${margin + 30},${margin / 2})`);
+        .attr('class', 'svg-content')
+        .attr("transform", function() {
+          if (graphAngle === 'right') {
+            return `translate(${margin - 30}, ${margin / 2})`;
+          } else if (graphAngle === 'top') {
+            return `translate(${margin + 10}, ${margin})`;
+          }
+          return `translate(${margin + 30},${margin / 2})`;
+        });
       return svg;
     },
     [height, width, margin, graphAngle]
@@ -73,45 +95,56 @@ const StackedBar = ({
     return color;
   }, [subgroups, colorPalette, colorType]);
 
+  const handleXAxis = useCallback((svgContent, x) => {
+      // x axis
+      if (graphAngle === 'bottom') {
+        svgContent
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x).tickSizeOuter(0));
+      } else if (graphAngle === 'top') {
+        svgContent
+        .append("g")
+        .attr("transform", `translate(0, 0)`)
+        .call(d3.axisTop(x).tickSizeOuter(0));
+      } else {
+        svgContent
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
+      }
+  }, [height, graphAngle]);
+
+  const handleYAxis = useCallback((svgContent, y) => {
+    if (graphAngle === 'bottom' || graphAngle === 'top') {
+      svgContent.append("g").call(d3.axisLeft(y));
+    } else if (graphAngle === 'right') {
+      svgContent
+      .append("g")
+      .attr("transform", `translate(${width}, 0)`)
+      .call(d3.axisRight(y).tickSizeOuter(0));
+    } else {
+      svgContent
+        .append("g")
+        .call(d3.axisLeft(y).tickSizeOuter(0));
+    }
+  }, [width, graphAngle]);
+
+
   const createPieGraph = useCallback(
     (div) => {
       const svg = drawSvg(div);
       const groups = data.map((d) => d[groupKey]);
       const stackedData = d3.stack().keys(subgroups)(data);
-      const [x, y] = handleAxis(groups);
       const color = handleScale();
 
-      // y axis
-      if (graphAngle === 'bottom' || graphAngle === 'top') {
-        svg.append("g").call(d3.axisLeft(y));
-      } else if (graphAngle === 'right') {
-        svg
-        .append("g")
-        .attr("transform", `translate(${width}, 0)`)
-        .call(d3.axisRight(y).tickSizeOuter(0));
-      } else {
-        svg
-          .append("g")
-          .call(d3.axisLeft(y).tickSizeOuter(0));
-      }
+      const [x, y] = handleAxis(groups);
       // x axis
-      if (graphAngle === 'bottom') {
-        svg
-        .append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0));
-      } else if (graphAngle === 'top') {
-        svg
-        .append("g")
-        .attr("transform", `translate(0, 0)`)
-        .call(d3.axisTop(x).tickSizeOuter(0));
-      } else {
-        svg
-        .append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
-      }
+      handleXAxis(svg, x);
+      // y axis
+      handleYAxis(svg, y);
 
+      // adding bars
       svg
         .append("g")
         .selectAll("g")
@@ -154,8 +187,44 @@ const StackedBar = ({
           } else {
             return y.bandwidth();
           }
-        });
+        })
+        .on('mouseover', function(event, d) {
+          // hover
+          if (checked) {
+            d3.select(this).transition()
+              .duration('50')
+              .attr('opacity', '.85')
 
+            svg.append('text')
+              .attr('class', 'hover-text')
+              .attr('x',  function() {
+                if (graphAngle === 'right') {
+                  return width-x(d[1]) - 30;
+                } else if (graphAngle === 'bottom' || graphAngle === 'top') {
+                  return x(d.data[groupKey]) + 25;
+                }
+                return x(d[0]) - 30;
+              })
+              .attr('y', function() {
+                if (graphAngle === 'bottom') {
+                  return y(d[1]) + 20;
+                } else if (graphAngle === 'top') {
+                  return y(d[1]) === 0 ? height - y(d[0]) + 20 : 0 + 20;
+                }
+                return y(d.data[groupKey]) + 45;
+              })
+              .text(d[1] - d[0])
+          }
+        })
+        .on('mouseout', function(d, i) {
+          d3.select(this).transition()
+          .duration('50')
+          .attr('opacity', '1')
+
+          svg.selectAll('.hover-text').remove();
+        })
+
+      // labels
       label.show &&
         svg
           .append("text")
@@ -163,7 +232,7 @@ const StackedBar = ({
           .attr("x", -height / 2)
           .attr("y", function() {
             if (graphAngle === 'right') {
-              return width + 14;
+              return width + margin;
             }
             return -margin / 1.3;
           })
@@ -171,9 +240,9 @@ const StackedBar = ({
           .attr("text-anchor", "middle")
           .text(function() {
             if (graphAngle === 'bottom' || graphAngle === 'top') {
-              return label.xLabel;
+              return labels.xLabel;
             }
-            return label.yLabel;
+            return labels.yLabel;
           });
 
       label.show &&
@@ -183,16 +252,16 @@ const StackedBar = ({
           .attr("x", width / 2)
           .attr("y", function() {
             if (graphAngle === 'top') {
-              return 0;
+              return -margin + 30;
             }
             return height + (margin * 2) / 2.5;
           })
           .attr("text-anchor", "middle")
           .text(function() {
             if (graphAngle === 'bottom' || graphAngle === 'top') {
-              return label.yLabel;
+              return labels.yLabel;
             }
-            return label.xLabel;
+            return labels.xLabel;
           });
     },
     [
@@ -205,7 +274,9 @@ const StackedBar = ({
       label,
       margin,
       width,
-      graphAngle
+      graphAngle,
+      labels,
+      checked
     ]
   );
 
@@ -217,7 +288,12 @@ const StackedBar = ({
 
   return (
     <div className="horizontal-stacked-bar-wrapper">
-      <StackedBarPanel handleClick={handleClick} />
+      <StackedBarPanel 
+        handleClick={handleClick} 
+        handleLabel={handleLabel} 
+        labels={labels}
+        hover={{ checked, setChecked }}
+        />
       <div className="horizontal-stacked-bar-right">
         <svg ref={svgRef} />
       </div>
--- src/components/StackedBar/StackedBarPanel.jsx
+++ src/components/StackedBar/StackedBarPanel.jsx
@@ -1,18 +1,74 @@
+import React, { useState } from 'react';
 import './StackedBarPanel.scss'
+import Switch from 'react-switch';
 
-const StackedBarPanel = ({ handleClick }) => {
+
+const StackedBarPanel = ({ handleClick, handleLabel, labels, hover: { checked, setChecked } }) => {
   const btns = ['right', 'left', 'top', 'bottom'];
+
+  const handleSwitch = (nextChecked) => {
+    setChecked(nextChecked);
+  }
 
   const handleRotate = (e) => {
     const angle = e.target.getAttribute('data-rotate');
     handleClick(angle);
   }
+
+  const onChangeLabel = (e) => {
+    const type = e.target.name;
+    const value = e.target.value;
+    handleLabel(type, value);
+  };
+
     return (
         <div className="horizontal-stacked-bar-left">
-          <div>
+          <div className="panel-item">
+            <h2>{"Rotate Graph"}</h2>
             {btns.map(btn => (
               <button data-rotate={btn} key={btn} onClick={handleRotate}>{btn}</button>
             ))}
+          </div>
+          <div className="panel-item">
+            <h2>{"Color"}</h2>
+          </div>
+          <div className="panel-item">
+            <h2>{"Max Value"}</h2>
+          </div>
+          <div className="panel-item">
+            <h2>{"Min Value"}</h2>
+          </div>
+          <div className="panel-item">
+            <h2>{"Hover"}</h2>
+            <Switch 
+              onChange={handleSwitch}
+              checked={checked}
+            />
+          </div>
+          <div className="panel-item">
+            <h2>{"Labels"}</h2>
+            <div>
+              <label>{"xLabel"}</label>
+              <input 
+                name="xLabel"
+                onChange={onChangeLabel}
+                type="text" 
+                value={labels?.xLabel} 
+              />
+            </div>
+            <div>
+              <label>{"yLabel"}</label>
+              <input 
+                name="yLabel"
+                onChange={onChangeLabel}
+                type="text" 
+                value={labels?.yLabel} 
+              />
+            </div>
+          </div>
+          <div className="panel-item">
+            <h2>{"Data"}</h2>
+            // TODO: dropdown? for csv
           </div>
         </div>
     )
